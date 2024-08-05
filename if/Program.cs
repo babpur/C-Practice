133508f4deabb919dd7ea6e395ac735ea1ff9999
babpur <namoonlvo@gmail.com>
2024-08-05 10:41:04 +0900

    c#에서 조건이 하나일 경우 if, 조건이 두 개일 경우 if-else, 조건이 세 개일 경우 if-else if-else를 각각 실습

---
+++ if/Program.cs
@@ -0,0 +1,30 @@
+﻿int num = 10;
+
+if (num == 10)
+{
+    Console.WriteLine("입력한 숫자는 10입니다.");
+}
+else if (num == 9)
+{
+    Console.WriteLine("입력한 숫자는 9입니다.");
+}
+else
+{
+    Console.WriteLine("입력한 숫자는 10 또는 9가 아닙니다.");
+}
+
+if (num > 0)
+{
+    Console.WriteLine("양수입니다.");
+}
+else if (num < 0)
+{
+    Console.WriteLine("음수입니다.");
+}
+else
+{
+    Console.WriteLine("0입니다.");
+}
+
+Console.WriteLine("프로그램을 종료하려면 엔터 키를 누르세요.");
+Console.ReadLine();
