6028a52b18793ea01daa6c603bf6d3f5c4254b2e
babpur <namoonlvo@gmail.com>
2024-08-05 15:42:03 +0900

    try-catch 구문을 사용하여 예외 처리.

---
+++ exceptionTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exceptionTest
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+            Console.Write("나눌 숫자를 입력하세요: ");
+            int num = int.Parse(Console.ReadLine());
+
+            try{
+                Console.WriteLine(10 / num);
+                
+            } catch(Exception e) {
+                Console.WriteLine("예외: " + e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
