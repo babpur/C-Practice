c355b74dd718ce75e9cd03a632ca87c7d0ec100f
babpur <namoonlvo@gmail.com>
2024-08-05 13:36:18 +0900

    상속과 오버라이딩에 대해 실습

---
+++ inheritanceTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Robot {
+    public void move() {
+        Console.WriteLine("로봇이 움직입니다.");
+    }
+}
+
+class CleanRobot : Robot{ // CleanRobot이 Robot을 상속받음
+    public void move() {
+        Console.WriteLine("청소 로봇이 움직입니다.");
+
+    }
+}
+
+class RescueRobot : Robot{ // RescueRobot이 Robot을 상속받음
+    public void move()
+    {
+        Console.WriteLine("구조 로봇이 움직입니다.");
+
+    }
+}
+
+
+class Program {
+        public static void Main(string[] args){
+            CleanRobot cr = new CleanRobot();
+            RescueRobot rr = new RescueRobot();
+            cr.move();
+            rr.move();
+            Console.ReadLine();
+        }
+    }
